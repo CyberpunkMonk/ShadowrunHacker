cc73ac5bb429d8c207e02b894d6e2a0504d75c11
Connor Boesch <Connor_Boesch@yahoo.com>
2016-04-13 23:14:05 -0500

    Added C# code to repo.

---
+++ ShadowrunHacker/Form1.Designer.cs
@@ -0,0 +1,375 @@
+﻿namespace ShadowrunHacker {
+	partial class Panel {
+		/// <summary>
+		/// Required designer variable.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Clean up any resources being used.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing) {
+			if (disposing && (components != null)) {
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows Form Designer generated code
+
+		/// <summary>
+		/// Required method for Designer support - do not modify
+		/// the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent() {
+			this.components = new System.ComponentModel.Container();
+			this.tableLayoutPanelButtons = new System.Windows.Forms.TableLayoutPanel();
+			this.button3 = new System.Windows.Forms.Button();
+			this.button2 = new System.Windows.Forms.Button();
+			this.button1 = new System.Windows.Forms.Button();
+			this.button6 = new System.Windows.Forms.Button();
+			this.button5 = new System.Windows.Forms.Button();
+			this.button4 = new System.Windows.Forms.Button();
+			this.button9 = new System.Windows.Forms.Button();
+			this.button8 = new System.Windows.Forms.Button();
+			this.button7 = new System.Windows.Forms.Button();
+			this.tableLayoutPanelLEDS = new System.Windows.Forms.TableLayoutPanel();
+			this.LED7 = new System.Windows.Forms.Button();
+			this.LED6 = new System.Windows.Forms.Button();
+			this.LED5 = new System.Windows.Forms.Button();
+			this.LED4 = new System.Windows.Forms.Button();
+			this.LED3 = new System.Windows.Forms.Button();
+			this.LED2 = new System.Windows.Forms.Button();
+			this.LED1 = new System.Windows.Forms.Button();
+			this.tableLayoutPanelBreakout = new System.Windows.Forms.TableLayoutPanel();
+			this.buttonStart = new System.Windows.Forms.Button();
+			this.timer1 = new System.Windows.Forms.Timer(this.components);
+			this.timerLabel = new System.Windows.Forms.Label();
+			this.tableLayoutPanelButtons.SuspendLayout();
+			this.tableLayoutPanelLEDS.SuspendLayout();
+			this.tableLayoutPanelBreakout.SuspendLayout();
+			this.SuspendLayout();
+			// 
+			// tableLayoutPanelButtons
+			// 
+			this.tableLayoutPanelButtons.AutoSize = true;
+			this.tableLayoutPanelButtons.CellBorderStyle = System.Windows.Forms.TableLayoutPanelCellBorderStyle.Outset;
+			this.tableLayoutPanelButtons.ColumnCount = 3;
+			this.tableLayoutPanelButtons.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 33.33333F));
+			this.tableLayoutPanelButtons.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 33.33334F));
+			this.tableLayoutPanelButtons.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 33.33334F));
+			this.tableLayoutPanelButtons.Controls.Add(this.button3, 2, 2);
+			this.tableLayoutPanelButtons.Controls.Add(this.button2, 1, 2);
+			this.tableLayoutPanelButtons.Controls.Add(this.button1, 0, 2);
+			this.tableLayoutPanelButtons.Controls.Add(this.button6, 2, 1);
+			this.tableLayoutPanelButtons.Controls.Add(this.button5, 1, 1);
+			this.tableLayoutPanelButtons.Controls.Add(this.button4, 0, 1);
+			this.tableLayoutPanelButtons.Controls.Add(this.button9, 2, 0);
+			this.tableLayoutPanelButtons.Controls.Add(this.button8, 1, 0);
+			this.tableLayoutPanelButtons.Controls.Add(this.button7, 0, 0);
+			this.tableLayoutPanelButtons.Location = new System.Drawing.Point(12, 73);
+			this.tableLayoutPanelButtons.Name = "tableLayoutPanelButtons";
+			this.tableLayoutPanelButtons.RowCount = 3;
+			this.tableLayoutPanelButtons.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 33.33333F));
+			this.tableLayoutPanelButtons.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 33.33333F));
+			this.tableLayoutPanelButtons.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 33.33333F));
+			this.tableLayoutPanelButtons.Size = new System.Drawing.Size(488, 428);
+			this.tableLayoutPanelButtons.TabIndex = 0;
+			// 
+			// button3
+			// 
+			this.button3.Dock = System.Windows.Forms.DockStyle.Fill;
+			this.button3.Location = new System.Drawing.Point(328, 289);
+			this.button3.Name = "button3";
+			this.button3.Size = new System.Drawing.Size(155, 134);
+			this.button3.TabIndex = 8;
+			this.button3.Text = "3";
+			this.button3.UseVisualStyleBackColor = false;
+			this.button3.Click += new System.EventHandler(this.ButtonClickedHandler);
+			// 
+			// button2
+			// 
+			this.button2.Dock = System.Windows.Forms.DockStyle.Fill;
+			this.button2.Location = new System.Drawing.Point(166, 289);
+			this.button2.Name = "button2";
+			this.button2.Size = new System.Drawing.Size(154, 134);
+			this.button2.TabIndex = 7;
+			this.button2.Text = "2";
+			this.button2.UseVisualStyleBackColor = false;
+			this.button2.Click += new System.EventHandler(this.ButtonClickedHandler);
+			// 
+			// button1
+			// 
+			this.button1.Dock = System.Windows.Forms.DockStyle.Fill;
+			this.button1.Location = new System.Drawing.Point(5, 289);
+			this.button1.Name = "button1";
+			this.button1.Size = new System.Drawing.Size(153, 134);
+			this.button1.TabIndex = 6;
+			this.button1.Text = "1";
+			this.button1.UseVisualStyleBackColor = false;
+			this.button1.Click += new System.EventHandler(this.ButtonClickedHandler);
+			// 
+			// button6
+			// 
+			this.button6.Dock = System.Windows.Forms.DockStyle.Fill;
+			this.button6.Location = new System.Drawing.Point(328, 147);
+			this.button6.Name = "button6";
+			this.button6.Size = new System.Drawing.Size(155, 134);
+			this.button6.TabIndex = 5;
+			this.button6.Text = "6";
+			this.button6.UseVisualStyleBackColor = false;
+			this.button6.Click += new System.EventHandler(this.ButtonClickedHandler);
+			// 
+			// button5
+			// 
+			this.button5.Dock = System.Windows.Forms.DockStyle.Fill;
+			this.button5.Location = new System.Drawing.Point(166, 147);
+			this.button5.Name = "button5";
+			this.button5.Size = new System.Drawing.Size(154, 134);
+			this.button5.TabIndex = 4;
+			this.button5.Text = "5";
+			this.button5.UseVisualStyleBackColor = false;
+			this.button5.Click += new System.EventHandler(this.ButtonClickedHandler);
+			// 
+			// button4
+			// 
+			this.button4.Dock = System.Windows.Forms.DockStyle.Fill;
+			this.button4.Location = new System.Drawing.Point(5, 147);
+			this.button4.Name = "button4";
+			this.button4.Size = new System.Drawing.Size(153, 134);
+			this.button4.TabIndex = 3;
+			this.button4.Text = "4";
+			this.button4.UseVisualStyleBackColor = false;
+			this.button4.Click += new System.EventHandler(this.ButtonClickedHandler);
+			// 
+			// button9
+			// 
+			this.button9.Dock = System.Windows.Forms.DockStyle.Fill;
+			this.button9.Location = new System.Drawing.Point(328, 5);
+			this.button9.Name = "button9";
+			this.button9.Size = new System.Drawing.Size(155, 134);
+			this.button9.TabIndex = 2;
+			this.button9.Text = "9";
+			this.button9.UseVisualStyleBackColor = false;
+			this.button9.Click += new System.EventHandler(this.ButtonClickedHandler);
+			// 
+			// button8
+			// 
+			this.button8.Dock = System.Windows.Forms.DockStyle.Fill;
+			this.button8.Location = new System.Drawing.Point(166, 5);
+			this.button8.Name = "button8";
+			this.button8.Size = new System.Drawing.Size(154, 134);
+			this.button8.TabIndex = 1;
+			this.button8.Text = "8";
+			this.button8.UseVisualStyleBackColor = false;
+			this.button8.Click += new System.EventHandler(this.ButtonClickedHandler);
+			// 
+			// button7
+			// 
+			this.button7.Dock = System.Windows.Forms.DockStyle.Fill;
+			this.button7.Location = new System.Drawing.Point(5, 5);
+			this.button7.Name = "button7";
+			this.button7.Size = new System.Drawing.Size(153, 134);
+			this.button7.TabIndex = 0;
+			this.button7.Text = "7";
+			this.button7.UseVisualStyleBackColor = false;
+			this.button7.Click += new System.EventHandler(this.ButtonClickedHandler);
+			// 
+			// tableLayoutPanelLEDS
+			// 
+			this.tableLayoutPanelLEDS.ColumnCount = 7;
+			this.tableLayoutPanelLEDS.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 14.28571F));
+			this.tableLayoutPanelLEDS.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 14.28571F));
+			this.tableLayoutPanelLEDS.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 14.28571F));
+			this.tableLayoutPanelLEDS.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 14.28571F));
+			this.tableLayoutPanelLEDS.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 14.28571F));
+			this.tableLayoutPanelLEDS.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 14.28571F));
+			this.tableLayoutPanelLEDS.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 14.28571F));
+			this.tableLayoutPanelLEDS.Controls.Add(this.LED7, 6, 0);
+			this.tableLayoutPanelLEDS.Controls.Add(this.LED6, 5, 0);
+			this.tableLayoutPanelLEDS.Controls.Add(this.LED5, 4, 0);
+			this.tableLayoutPanelLEDS.Controls.Add(this.LED4, 3, 0);
+			this.tableLayoutPanelLEDS.Controls.Add(this.LED3, 2, 0);
+			this.tableLayoutPanelLEDS.Controls.Add(this.LED2, 1, 0);
+			this.tableLayoutPanelLEDS.Controls.Add(this.LED1, 0, 0);
+			this.tableLayoutPanelLEDS.Dock = System.Windows.Forms.DockStyle.Top;
+			this.tableLayoutPanelLEDS.Location = new System.Drawing.Point(0, 0);
+			this.tableLayoutPanelLEDS.Name = "tableLayoutPanelLEDS";
+			this.tableLayoutPanelLEDS.RowCount = 1;
+			this.tableLayoutPanelLEDS.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 100F));
+			this.tableLayoutPanelLEDS.Size = new System.Drawing.Size(499, 67);
+			this.tableLayoutPanelLEDS.TabIndex = 0;
+			// 
+			// LED7
+			// 
+			this.LED7.BackColor = System.Drawing.Color.Red;
+			this.LED7.Dock = System.Windows.Forms.DockStyle.Fill;
+			this.LED7.Enabled = false;
+			this.LED7.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+			this.LED7.Location = new System.Drawing.Point(429, 3);
+			this.LED7.Name = "LED7";
+			this.LED7.Size = new System.Drawing.Size(67, 61);
+			this.LED7.TabIndex = 6;
+			this.LED7.UseVisualStyleBackColor = false;
+			// 
+			// LED6
+			// 
+			this.LED6.BackColor = System.Drawing.Color.Red;
+			this.LED6.Dock = System.Windows.Forms.DockStyle.Fill;
+			this.LED6.Enabled = false;
+			this.LED6.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+			this.LED6.Location = new System.Drawing.Point(358, 3);
+			this.LED6.Name = "LED6";
+			this.LED6.Size = new System.Drawing.Size(65, 61);
+			this.LED6.TabIndex = 5;
+			this.LED6.UseVisualStyleBackColor = false;
+			// 
+			// LED5
+			// 
+			this.LED5.BackColor = System.Drawing.Color.Red;
+			this.LED5.Dock = System.Windows.Forms.DockStyle.Fill;
+			this.LED5.Enabled = false;
+			this.LED5.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+			this.LED5.Location = new System.Drawing.Point(287, 3);
+			this.LED5.Name = "LED5";
+			this.LED5.Size = new System.Drawing.Size(65, 61);
+			this.LED5.TabIndex = 4;
+			this.LED5.UseVisualStyleBackColor = false;
+			// 
+			// LED4
+			// 
+			this.LED4.BackColor = System.Drawing.Color.Red;
+			this.LED4.Dock = System.Windows.Forms.DockStyle.Fill;
+			this.LED4.Enabled = false;
+			this.LED4.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+			this.LED4.Location = new System.Drawing.Point(216, 3);
+			this.LED4.Name = "LED4";
+			this.LED4.Size = new System.Drawing.Size(65, 61);
+			this.LED4.TabIndex = 3;
+			this.LED4.UseVisualStyleBackColor = false;
+			// 
+			// LED3
+			// 
+			this.LED3.BackColor = System.Drawing.Color.Red;
+			this.LED3.Dock = System.Windows.Forms.DockStyle.Fill;
+			this.LED3.Enabled = false;
+			this.LED3.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+			this.LED3.Location = new System.Drawing.Point(145, 3);
+			this.LED3.Name = "LED3";
+			this.LED3.Size = new System.Drawing.Size(65, 61);
+			this.LED3.TabIndex = 2;
+			this.LED3.UseVisualStyleBackColor = false;
+			// 
+			// LED2
+			// 
+			this.LED2.BackColor = System.Drawing.Color.Red;
+			this.LED2.Dock = System.Windows.Forms.DockStyle.Fill;
+			this.LED2.Enabled = false;
+			this.LED2.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+			this.LED2.Location = new System.Drawing.Point(74, 3);
+			this.LED2.Name = "LED2";
+			this.LED2.Size = new System.Drawing.Size(65, 61);
+			this.LED2.TabIndex = 1;
+			this.LED2.UseVisualStyleBackColor = false;
+			// 
+			// LED1
+			// 
+			this.LED1.BackColor = System.Drawing.Color.Red;
+			this.LED1.Dock = System.Windows.Forms.DockStyle.Fill;
+			this.LED1.Enabled = false;
+			this.LED1.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+			this.LED1.Location = new System.Drawing.Point(3, 3);
+			this.LED1.Name = "LED1";
+			this.LED1.Size = new System.Drawing.Size(65, 61);
+			this.LED1.TabIndex = 0;
+			this.LED1.UseVisualStyleBackColor = false;
+			// 
+			// tableLayoutPanelBreakout
+			// 
+			this.tableLayoutPanelBreakout.ColumnCount = 2;
+			this.tableLayoutPanelBreakout.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+			this.tableLayoutPanelBreakout.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+			this.tableLayoutPanelBreakout.Controls.Add(this.buttonStart, 0, 0);
+			this.tableLayoutPanelBreakout.Controls.Add(this.timerLabel, 1, 0);
+			this.tableLayoutPanelBreakout.Dock = System.Windows.Forms.DockStyle.Bottom;
+			this.tableLayoutPanelBreakout.Location = new System.Drawing.Point(0, 505);
+			this.tableLayoutPanelBreakout.Name = "tableLayoutPanelBreakout";
+			this.tableLayoutPanelBreakout.RowCount = 1;
+			this.tableLayoutPanelBreakout.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
+			this.tableLayoutPanelBreakout.Size = new System.Drawing.Size(499, 83);
+			this.tableLayoutPanelBreakout.TabIndex = 0;
+			// 
+			// buttonStart
+			// 
+			this.buttonStart.BackColor = System.Drawing.Color.Teal;
+			this.buttonStart.Dock = System.Windows.Forms.DockStyle.Fill;
+			this.buttonStart.Location = new System.Drawing.Point(3, 3);
+			this.buttonStart.Name = "buttonStart";
+			this.buttonStart.Size = new System.Drawing.Size(243, 77);
+			this.buttonStart.TabIndex = 0;
+			this.buttonStart.Text = "START (0)";
+			this.buttonStart.UseVisualStyleBackColor = false;
+			this.buttonStart.Click += new System.EventHandler(this.buttonStart_Click);
+			// 
+			// timer1
+			// 
+			this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+			// 
+			// timerLabel
+			// 
+			this.timerLabel.AutoSize = true;
+			this.timerLabel.Location = new System.Drawing.Point(252, 0);
+			this.timerLabel.Name = "timerLabel";
+			this.timerLabel.Size = new System.Drawing.Size(127, 20);
+			this.timerLabel.TabIndex = 1;
+			this.timerLabel.Text = "Time Remaining:";
+			// 
+			// Panel
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.BackColor = System.Drawing.Color.Teal;
+			this.ClientSize = new System.Drawing.Size(499, 588);
+			this.Controls.Add(this.tableLayoutPanelBreakout);
+			this.Controls.Add(this.tableLayoutPanelLEDS);
+			this.Controls.Add(this.tableLayoutPanelButtons);
+			this.Name = "Panel";
+			this.Text = "Shadowrun Hacker";
+			this.Load += new System.EventHandler(this.Panel_Load);
+			this.tableLayoutPanelButtons.ResumeLayout(false);
+			this.tableLayoutPanelLEDS.ResumeLayout(false);
+			this.tableLayoutPanelBreakout.ResumeLayout(false);
+			this.tableLayoutPanelBreakout.PerformLayout();
+			this.ResumeLayout(false);
+			this.PerformLayout();
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.TableLayoutPanel tableLayoutPanelButtons;
+		private System.Windows.Forms.TableLayoutPanel tableLayoutPanelLEDS;
+		private System.Windows.Forms.TableLayoutPanel tableLayoutPanelBreakout;
+		private System.Windows.Forms.Button button3;
+		private System.Windows.Forms.Button button2;
+		private System.Windows.Forms.Button button1;
+		private System.Windows.Forms.Button button6;
+		private System.Windows.Forms.Button button5;
+		private System.Windows.Forms.Button button4;
+		private System.Windows.Forms.Button button9;
+		private System.Windows.Forms.Button button8;
+		private System.Windows.Forms.Button button7;
+		private System.Windows.Forms.Button buttonStart;
+		private System.Windows.Forms.Button LED7;
+		private System.Windows.Forms.Button LED6;
+		private System.Windows.Forms.Button LED5;
+		private System.Windows.Forms.Button LED4;
+		private System.Windows.Forms.Button LED3;
+		private System.Windows.Forms.Button LED2;
+		private System.Windows.Forms.Button LED1;
+		private System.Windows.Forms.Timer timer1;
+		private System.Windows.Forms.Label timerLabel;
+	}
+}
+
---
+++ ShadowrunHacker/Form1.cs
@@ -0,0 +1,142 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Windows.Forms;
+
+namespace ShadowrunHacker {
+	public partial class Panel : Form {
+		int dc = 0;//Difficulty Class
+		int position = 0;
+		List<int> pattern = new List<int>();
+		Dictionary<int,Button> buttons = new Dictionary<int,Button>();
+		List<Button> LEDs = new List<Button>();
+		Random rand = new Random();
+		bool playback = false;
+		double time;
+		public Panel() {
+			InitializeComponent();
+		}
+
+		private void GUIPlayback() {
+			playback = true;
+			foreach (int i in pattern) {
+				Thread.Sleep(200);
+				blinkenight(buttons[i],Color.DarkBlue);
+			}
+			playback = false;
+		}
+
+		private void GUIPlayback(int i, Color c) {
+			playback = true;
+			blinkenight(buttons[i], c);
+			playback = false;
+		}
+
+		private void testCorrect(int buttonPressed) {
+			if (playback) return;
+			if (pattern!=null && pattern[position] == buttonPressed) position++;
+			else {
+				//IF THE WRONG BUTTON IS PRESSED
+				//Notify user of wrong button pressed via button flash
+				//blinkenight(buttons[buttonPressed], Color.Red);//NOT CURRENTLY WORKING
+				//Wait some time as punishment.
+				//Thread.Sleep(200);//NOT CURRENTLY WORKING?
+				reset();//Reset may not ben working
+				//BUZZER NOIZE OR SOMETHING?
+				new Thread(GUIPlayback).Start();
+			}
+			if (position >= pattern.Count) {
+				LEDs[dc - 1].BackColor = Color.Green;
+				dc++;
+				reset();
+				if (dc == 8) {
+					timer1.Stop();
+					buttonStart.Enabled = true;
+					reset();
+					time = 45.0;
+					MessageBox.Show("CONGRATULATIONS", "YOU SOLVED ALL THE PUZZLES!");
+				}
+				else new Thread(GUIPlayback).Start();
+			}
+		}
+
+		private void blinkenight(Button b, Color c) {
+			b.BackColor = c;
+			Thread.Sleep(200);
+			b.BackColor = Color.Teal;
+		}
+
+		private void Panel_Load(object sender, EventArgs e) {
+			buttons.Add(1, button1); buttons.Add(2, button2); buttons.Add(3, button3);
+			buttons.Add(4, button4); buttons.Add(5, button5); buttons.Add(6, button6);
+			buttons.Add(7, button7); buttons.Add(8, button8); buttons.Add(9, button9);
+			LEDs.Add(LED1); LEDs.Add(LED2); LEDs.Add(LED3); LEDs.Add(LED4); LEDs.Add(LED5);
+			LEDs.Add(LED6); LEDs.Add(LED7);
+			time = 45.0;
+		}
+
+		private void level() {
+			int temp = 0;
+			if (dc < 1 || dc > 8) dc = 1;
+			if (dc <= 2 && dc >= 1)	temp = 4;
+			else if (dc <= 4 && dc >= 3) temp = 5;
+			else if (dc <= 6 && dc >= 5) temp = 6;
+			else temp = 7;
+			for (int i = 0; i < temp; i++) pattern.Add(rand.Next(1, 10));
+		}
+
+		private void buttonStart_Click(object sender, EventArgs e) {
+			resetLEDs();
+			buttonStart.Enabled = false;
+			dc++;
+			pattern = new List<int>();
+			level();
+			timer1.Start();
+			new Thread(GUIPlayback).Start();
+			
+		}
+
+		private void reset() {
+			//Do I want to change the dc on a reset?
+			//int dc = 0;
+			position = 0;
+			pattern = null;
+			pattern = new List<int>();
+			level();
+			//Random rand = new Random();
+			//bool playback = false;
+		}
+
+		private void ButtonClickedHandler(object sender, EventArgs e){
+			Button b = sender as Button;
+			if(pattern.Count>0)testCorrect(Int32.Parse(b.Text));
+		}
+
+		private void LEDHandler() {
+			if (dc > 6 || dc < 1) return;
+			LEDs[dc - 1].BackColor = Color.Green;
+		}
+
+		private void resetLEDs() {
+			foreach (Button LED in LEDs) LED.BackColor = Color.Red;
+		}
+
+		private void timer1_Tick(object sender, EventArgs e){
+			if (time < .05) {
+				timer1.Stop();
+				buttonStart.Enabled = true;
+				reset();
+				time = 45.0;
+				MessageBox.Show("TOO BAD","YOU RAN OUT OF TIME!");
+			}
+			time -= .1;
+			string output = String.Format("Time Remaining: {0:0.00}", time);
+			timerLabel.Text = output;
+		}
+	}
+}
